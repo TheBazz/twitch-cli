1bac925521077ee768335ffda8f941afea695a40
TheBazz <paul.chambazz@gmail.com>
2021-12-16 05:41:58 +0100

    twitch-cli works again

--- source/main.c
+++ source/main.c
@@ -7,14 +7,16 @@
 #include <pwd.h>
 
 #define MAX_STREAMERS 256
+#define MAX_ANSWER 256
 #define KW_LIVE "live_user"
 #define KW_EXIST "rel=\"alt"
 
 #define RESET "\033[0;0m"
 #define RED "\033[0;31m"
 #define GREEN "\033[0;32m"
-#define BOLD "\033[1m"
 #define GREY "\033[0;37m"
+#define BOLD "\033[1m"
+#define UNDER "\033[4m"
 
 static size_t WriteMemoryCallback (void *content, size_t size, size_t nmemb, void *userp);
 
@@ -24,16 +26,6 @@ struct MemoryStruct {
 };
 
 int main (int argc, char *argv[]) {
-  size_t pathEnd;
-  for (size_t i = strlen(argv[0]); i != 0; --i) {
-    if (argv[0][i] == '/') {
-      pathEnd = i;
-      break;
-    }
-  }
-  char *path = (char *) malloc(sizeof(char) * pathEnd);
-  strncpy(path, argv[0], pathEnd);
-
   struct passwd *pw = getpwuid(getuid());
   char *homePath = pw->pw_dir;
   char *streamerPath = "/.config/twitch-cli/streamers.txt";
@@ -44,7 +36,7 @@ int main (int argc, char *argv[]) {
   }
 
   printf("%sYour twitch channels : %s\n", BOLD, RESET);
-  short count = 0;
+  int count = 0;
   char **streamers = (char **) malloc(sizeof(char *) * MAX_STREAMERS);
   char *line = NULL;
   size_t len = 0;
@@ -64,19 +56,19 @@ int main (int argc, char *argv[]) {
   chunk.memory = (char *) malloc(1);
   chunk.size = 0;
   char *address = "https://www.twitch.tv/";
-  for (short i = 0; i != count; ++i) {
+  for (int i = 0; i != count; ++i) {
     curl_global_init(CURL_GLOBAL_ALL);
     curl_handle = curl_easy_init();
     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
     curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
     char *url = (char *) malloc(sizeof(char) * (strlen(address) + strlen(streamers[i])));
     for (int j = 0; j != strlen(address); ++j) {
-      *(url + j) = *(address + j);
+      url[j] = address[j];
     }
     for (int j = 0; j != strlen(streamers[i]); ++j) {
-      *(url + j + strlen(address)) = *(streamers[i] + j);
+      url[j + strlen(address)] = streamers[i][j];
     }
-    *(url + strlen(address) + strlen(streamers[i])) = '\0';
+    url[strlen(address) + strlen(streamers[i])] = '\0';
     curl_easy_setopt(curl_handle, CURLOPT_URL, url);
     free(url);
     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
@@ -109,6 +101,40 @@ int main (int argc, char *argv[]) {
     }
   }
   printf(" %d - Exit\n", count + 1);
+  int answer = -1;
+  char str_answer[MAX_ANSWER];
+  do {
+    if (answer != -1 && answer <= 0 || answer > count + 1 || status[answer - 1] != 0) {
+      printf("Error. ");
+    }
+    printf("What do you want to do (1-%s%d%s)? : ", UNDER, count + 1, RESET);
+    fgets(str_answer, MAX_ANSWER, stdin);
+    str_answer[strlen(str_answer) - 1] = '\0';
+    if (strlen(str_answer) == 0) {
+      exit(EXIT_SUCCESS);
+    }
+    answer = atoi(str_answer);
+    if (answer == count + 1) {
+      exit(EXIT_SUCCESS);
+    }
+  } while (answer <= 0 || answer > count + 1 || status[answer - 1] != 0);
+  printf("Starting stream...");
+  char *start = "mpv --force-seekable=yes --speed=1 --really-quiet \"https://www.twitch.tv/";
+  char *out = (char *) malloc(sizeof(char) * (strlen(start) + strlen(streamers[answer - 1]) + 1));
+  for (int i = 0; i != strlen(start); ++i) {
+    out[i] = start[i];
+  }
+  for (int i = 0; i != strlen(streamers[answer - 1]); ++i) {
+    out[i + strlen(start)] = streamers[answer - 1][i];
+  }
+  out[strlen(start) + strlen(streamers[answer - 1])] = '"';
+  out[strlen(start) + strlen(streamers[answer - 1]) + 1] = '\0';
+  system(out);
+  for (int i = 0; i != count; ++i) {
+    free(streamers[i]);
+  }
+  free(streamers);
+  free(out);
 }
 
 static size_t WriteMemoryCallback (void *content, size_t size, size_t nmemb, void *userp) {
