b93e3358961c10d2c61ba8c7f30102c6049af642
TheBazz <paul.chambazz@gmail.com>
2021-12-16 05:52:11 +0100

    added comments

--- source/main.c
+++ source/main.c
@@ -36,6 +36,8 @@ int main (int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
+  // we read from the file and store all the streamers to get
+  // we also count the number of streamers
   int count = 0;
   char **streamers = (char **) malloc(sizeof(char *) * MAX_STREAMERS);
   char *line = NULL;
@@ -47,9 +49,12 @@ int main (int argc, char *argv[]) {
     streamers[count][read - 1] = '\0';
     count++;
   }
-  streamers = (char **) realloc(streamers, sizeof(char *) * count);
-  char *status = (char *) malloc(sizeof(char) * count);
   fclose(streamerFile);
+  streamers = (char **) realloc(streamers, sizeof(char *) * count);
+
+  // now that we have a list of all streamers, we use curl to get the html of the page
+  // we will then search some special characters in the html to determine if the user is live
+  char *status = (char *) malloc(sizeof(char) * count);
   CURL *curl_handle;
   CURLcode res;
   struct MemoryStruct chunk;
@@ -57,10 +62,11 @@ int main (int argc, char *argv[]) {
   chunk.size = 0;
   char *address = "https://www.twitch.tv/";
   for (int i = 0; i != count; ++i) {
+    // default settings for a curl request
     curl_global_init(CURL_GLOBAL_ALL);
     curl_handle = curl_easy_init();
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
     curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    // we build the url of the curl request manually
     char *url = (char *) malloc(sizeof(char) * (strlen(address) + strlen(streamers[i])));
     for (int j = 0; j != strlen(address); ++j) {
       url[j] = address[j];
@@ -71,7 +77,11 @@ int main (int argc, char *argv[]) {
     url[strlen(address) + strlen(streamers[i])] = '\0';
     curl_easy_setopt(curl_handle, CURLOPT_URL, url);
     free(url);
+    // we throw a call to WriteMemoryCallback, the function is defined below
+    // it will then be stored on our chunk structure
     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+    //we finally perform the request and deal with the result
     res = curl_easy_perform(curl_handle);
     if (res != CURLE_OK) {
       printf("Error, curl failed.");
@@ -79,6 +89,7 @@ int main (int argc, char *argv[]) {
       exit(EXIT_FAILURE);
     }
     char *output = chunk.memory;
+    // we search in the html of the page for the pattern
     if (strstr(output, KW_LIVE)) {
       status[i] = 0;
     } else if (strstr(output, KW_EXIST)) {
@@ -91,6 +102,7 @@ int main (int argc, char *argv[]) {
     chunk.size = 0;
   }
   free(chunk.memory);
+  // we print the result of the curl to the user
   printf("%sYour twitch channels : %s\n", BOLD, RESET);
   for (int i = 0; i != count; i++) {
     if (status[i] == 0) {
@@ -102,9 +114,11 @@ int main (int argc, char *argv[]) {
     }
   }
   printf(" %d - Exit\n", count + 1);
+  // we setup a response
   int answer = -1;
   char str_answer[MAX_ANSWER];
   do {
+    // if the user has chosen an out of range option, or a streamer who is not live, we print an error.
     if (answer != -1 && answer <= 0 || answer > count + 1 || status[answer - 1] != 0) {
       printf("Error. ");
     }
@@ -112,14 +126,17 @@ int main (int argc, char *argv[]) {
     fgets(str_answer, MAX_ANSWER, stdin);
     str_answer[strlen(str_answer) - 1] = '\0';
     if (strlen(str_answer) == 0) {
+      // we autoexit if the user has chosen nothing (default exit)
       exit(EXIT_SUCCESS);
     }
     answer = atoi(str_answer);
     if (answer == count + 1) {
+      // we also exit if the user has chosen the exit option
       exit(EXIT_SUCCESS);
     }
   } while (answer <= 0 || answer > count + 1 || status[answer - 1] != 0);
   printf("Starting stream...");
+  // we construct the string for the command with mpv
   char *start = "mpv --force-seekable=yes --speed=1 --really-quiet \"https://www.twitch.tv/";
   char *out = (char *) malloc(sizeof(char) * (strlen(start) + strlen(streamers[answer - 1]) + 1));
   for (int i = 0; i != strlen(start); ++i) {
@@ -130,7 +147,9 @@ int main (int argc, char *argv[]) {
   }
   out[strlen(start) + strlen(streamers[answer - 1])] = '"';
   out[strlen(start) + strlen(streamers[answer - 1]) + 1] = '\0';
+  // then run the command
   system(out);
+  // and free what's to be freed
   for (int i = 0; i != count; ++i) {
     free(streamers[i]);
   }
@@ -138,6 +157,7 @@ int main (int argc, char *argv[]) {
   free(out);
 }
 
+/* this function returns the string content of a website to a memorystruct */
 static size_t WriteMemoryCallback (void *content, size_t size, size_t nmemb, void *userp) {
   size_t realsize = size * nmemb;
   struct MemoryStruct *mem = (struct MemoryStruct *) userp;
